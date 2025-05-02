c6ff8782f6979103bba90bf0f743dc234f0e2b4a
temple646464 <Temple646464@gmail.com>
2025-05-02 12:21:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "27058143"
-api_hash = "c569ea4f5ade3fc70d2bb1fd162d9dc9"
-bot_token = "7822595866:AAEVZ0f6AGaOROtlGqFZoo1PD1sQFuPtoNQ"
-auth_users = [8144269730]
+api_id = "28748671"
+api_hash = "f53ec7c41ce34e6d585674ed9ce6167c"
+bot_token = "7791056995:AAGTt6xsnHrcST6V496hzHlht1RpO-be1lc"
+auth_users = [1169394017]
